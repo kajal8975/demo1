5d22d5e86066b509c3ff0f704089e35926e95931
Kajal Nikam <nikamkajal96@gmail.com>
2021-09-01 22:13:22 +0530

    added Topic list

--- demo/src/main/java/com/example/demo/TopicController.java
+++ demo/src/main/java/com/example/demo/TopicController.java
@@ -1,20 +1,27 @@
 package com.example.demo;
 
+import java.util.Arrays;
+import java.util.List;
+
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
     public class TopicController {
 
-    public static void main(String args[]) {
-        System.out.println("Sagar");
-    }
+
+
+    @RequestMapping("/Topics")
+        public List<Topics> getAllTopics() {
+            return Arrays.asList(
+                    new Topics("spring","spring framework","spring framework description"),
+                    new Topics("java","core java","core java description"),
+                    new Topics("javascript","javascript","javascript description")
 
 
 
-        @RequestMapping("/Topics")
-        public String getAllTopics() {
-            return "hii Sagar";
+                    );
         }
 
 }
